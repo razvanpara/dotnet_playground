3453c41041b7a6786809bc3da1044d1446e283a3
Razvan Paraschivescu <razvan.paraschivescu@ubisoft.com>
2021-12-09 18:24:10 +0200

    added screen background color

--- ConsoleSnakeGame/Display/ColorScreen.cs
+++ ConsoleSnakeGame/Display/ColorScreen.cs
@@ -5,14 +5,38 @@ namespace ConsoleSnakeGame.Display
 {
     public class ColorScreen : BWScreen
     {
+        ConsoleColor _background;
+        public ColorScreen(int width, int height, ConsoleColor color) : base(width, height)
+        {
+            _background = color;
+            SetBackground();
+        }
         public ColorScreen(int width, int height) : base(width, height)
         {
-
+            SetBackground();
         }
 
-        private void ResetColor() => Console.ForegroundColor = ConsoleColor.White;
+        private void SetBackground()
+        {
+            Console.BackgroundColor = _background;
+            for (int c = 0; c < Width; c++)
+                for (int r = 0; r < Height; r++)
+                    base.ClearPixel(new Point(c, r));
+        }
+        private void ResetColor()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public override void ClearPixel(Point point)
+        {
+            Console.BackgroundColor = _background;
+            base.ClearPixel(point);
+            ResetColor();
+        }
         public override void SetPixel(Point point)
         {
+            Console.BackgroundColor = _background;
             Console.ForegroundColor = point.Color;
             base.SetPixel(point);
             ResetColor();
